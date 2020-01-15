a4ec6c9fd3320131cb97a8279bd38992d3b89c96
Eamon Nerbonne <eamon@nerbonne.org>
2020-01-15 21:19:03 +0100

    Add really simple benchmark with pretty much empty projects; even here a significant regression; results:
    E:\vcs\EfCoreCompileBench [master ↑1]> dotnet run --project Benchmark\Benchmark.csproj
    Release310Rebuild: 0.7990629899999999 +/- 0.009089836825097489 seconds (including best time 0.7840217; excluding outliers 0.8307335; 0.8280571)
    
    Release311Rebuild: 1.20749151 +/- 0.009874841714523908 seconds (including best time 1.1859872; excluding outliers 1.225968; 1.2197148)
    
    Debug310Rebuild: 0.80531395 +/- 0.010476545098480696 seconds (including best time 0.779024; excluding outliers 0.8275574; 0.818614)
    
    Debug311Rebuild: 1.20266026 +/- 0.01071111190112402 seconds (including best time 1.1865318; excluding outliers 1.228329; 1.2187895)
    
    Debug310NoOpBuild: 0.66530095 +/- 0.013576504062184087 seconds (including best time 0.6419545; excluding outliers 0.6876717; 0.6875402)
    
    Debug311NoOpBuild: 0.66312895 +/- 0.009086628069559155 seconds (including best time 0.6493933; excluding outliers 0.687344; 0.6785388)
    
    Debug310BuildWithTouchedFile: 0.70639161 +/- 0.011510180214614352 seconds (including best time 0.6856614; excluding outliers 0.7343375; 0.7254502)
    
    Debug311BuildWithTouchedFile: 1.11351753 +/- 0.01382988313790468 seconds (including best time 1.0830584; excluding outliers 1.1413067; 1.13962)

---
+++ Benchmark/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using IncrementalMeanVarianceAccumulator;
+using ProgressOnderwijsUtils;
+
+namespace Benchmark
+{
+    class Program
+    {
+        const int NumberOfBuildsToBenchmark = 12;
+        const int SlowOutliersToDiscard = 2; //at least one, for caching effects.
+
+        static void Main()
+        {
+            Process.GetCurrentProcess().PriorityClass = ProcessPriorityClass.AboveNormal;
+
+            var results = (
+                from build in PredefinedBuilds.Builds
+                let times = Enumerable.Repeat(0, NumberOfBuildsToBenchmark).Select(_ => TimeBuild(build)).ToArray()
+                let worstToBest = times.OrderByDescending(s => s).ToArray()
+                let distributionWithoutOutliers = MeanVarianceAccumulator.FromSequence(worstToBest.Skip(SlowOutliersToDiscard))
+                let ignoredOutliers = worstToBest.Take(SlowOutliersToDiscard)
+                let best = worstToBest.Last()
+                select $"{build.Name}: {distributionWithoutOutliers} seconds (including best time {best}; excluding outliers {string.Join("; ", ignoredOutliers)})"
+            ).ToArray();
+
+            Console.WriteLine(string.Join("\r\n\r\n", results));
+        }
+
+        static readonly Uri solutionpath = GetMyPath();
+
+        static Uri GetMyPath([CallerFilePath] string path = null)
+            => new Uri(path).Combine(@"..\");
+
+        static class PredefinedBuilds
+        {
+            public static readonly BuildSettings[] Builds = {
+                new BuildSettings("Release310Rebuild") { Executable = "dotnet", Arguments = "msbuild Project310/Project310.csproj -target:restore;rebuild -verbosity:minimal -p:configuration=Release" },
+                new BuildSettings("Release311Rebuild") { Executable = "dotnet", Arguments = "msbuild Project311/Project311.csproj -target:restore;rebuild -verbosity:minimal -p:configuration=Release" },
+
+                new BuildSettings("Debug310Rebuild") { Executable = "dotnet", Arguments = "msbuild Project310/Project310.csproj -target:restore;rebuild -verbosity:minimal -p:configuration=Debug" },
+                new BuildSettings("Debug311Rebuild") { Executable = "dotnet", Arguments = "msbuild Project311/Project311.csproj -target:restore;rebuild -verbosity:minimal -p:configuration=Debug" },
+
+                new BuildSettings("Debug310NoOpBuild") { Executable = "dotnet", Arguments = "msbuild Project310/Project310.csproj -target:restore;build -verbosity:minimal -p:configuration=Debug" },
+                new BuildSettings("Debug311NoOpBuild") { Executable = "dotnet", Arguments = "msbuild Project311/Project311.csproj -target:restore;build -verbosity:minimal -p:configuration=Debug" },
+
+                new BuildSettings("Debug310BuildWithTouchedFile") {
+                    Prebuild = FileToucher("Project310/Program.cs"),
+                    Executable = "dotnet",
+                    Arguments = "msbuild Project310/Project310.csproj -target:restore;build -verbosity:minimal -p:configuration=Debug"
+                },
+                new BuildSettings("Debug311BuildWithTouchedFile") {
+                    Prebuild = FileToucher("Project311/Program.cs"),
+                    Executable = "dotnet",
+                    Arguments = "msbuild Project311/Project311.csproj -target:restore;build -verbosity:minimal -p:configuration=Debug"
+                }
+            };
+
+            static Action FileToucher(string solutionRelativeFilePath)
+                => () => File.SetLastWriteTimeUtc(solutionpath.Combine(solutionRelativeFilePath).LocalPath, DateTime.UtcNow);
+        }
+
+        class BuildSettings
+        {
+            public readonly string Name;
+
+            public BuildSettings(string name)
+                => Name = name;
+
+            public string Executable;
+            public string Arguments;
+            public Action Prebuild;
+
+            public override string ToString()
+                => Name;
+        }
+
+        static double TimeBuild(BuildSettings settings)
+        {
+            settings.Prebuild?.Invoke();
+            var sw = Stopwatch.StartNew();
+            var startInfo = new ProcessStartInfo {
+                WorkingDirectory = solutionpath.LocalPath,
+                FileName = settings.Executable,
+                Arguments = settings.Arguments,
+                CreateNoWindow = true,
+                RedirectStandardError = true,
+                RedirectStandardInput = true,
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+            };
+            using var proc = Process.Start(startInfo);
+            proc.StandardInput.Close();
+            proc.StandardOutput.ReadToEnd();
+            proc.WaitForExit();
+            return sw.Elapsed.TotalSeconds;
+        }
+    }
+}
